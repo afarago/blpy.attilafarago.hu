3d71eb1ac314ef5b75b7f0d66385b638fbb5057c
Attila Farago <attila.farago.hu@gmail.com>
2025-01-05 19:11:26 +0100

    fix: update footer alignment and styling for improved responsiveness

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer: React.FC = () => (
     <footer className="border-top footer text-muted">
-        <div className="mx-5">
+        <div className="mx-md-5 text-center text-md-end">
             &copy; Attila Faragó - <a href="https://github.com/afarago">@afarago</a> -{' '}
             <span title={GITHUB_VERSION}>2025</span>
         </div>
