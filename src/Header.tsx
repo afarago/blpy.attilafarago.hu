7e4792716b8c012654c59fcc82a762f587ea1d56
Attila Farago <attila.farago.hu@gmail.com>
2025-01-06 01:35:14 +0100

    feat: add logo click handler to reset conversion result and enhance header interactivity

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { MouseEventHandler, useContext, useEffect, useState } from 'react';
 
 import LogoFull from './img/logo_full.svg?react';
+import { MyContext } from './contexts/MyContext';
 
 const Header: React.FC = () => {
+    const context = useContext(MyContext);
+    if (!context) throw new Error('MyComponent must be used within a MyProvider');
+    const { setConversionResult } = context;
     const [isScrolled, setIsScrolled] = useState(false);
 
     useEffect(() => {
@@ -21,12 +25,22 @@ const Header: React.FC = () => {
         };
     }, []);
 
+    function handleClickOnLogo(event: React.MouseEvent<SVGSVGElement>): void {
+        event.preventDefault();
+        setConversionResult(undefined);
+    }
+
     return (
         <header className={'fixed-top ' + (isScrolled ? 'collapsed' : '')}>
             <nav className="navbar navbar-expand-sm border-bottom box-shadow mb-3">
                 <div className="container-md">
                     <div className="navbar-brand w-100">
-                        <LogoFull height="1.5em" width="4em" className="brandlogo" />
+                        <LogoFull
+                            height="1.5em"
+                            width="4em"
+                            className="brandlogo"
+                            onClick={handleClickOnLogo}
+                        />
                         &nbsp;
                         <b>BlocklyPy</b>
                         <span className="d-none d-sm-inline">&nbsp;· LegoAppTools</span>
