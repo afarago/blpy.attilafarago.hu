c0df2aa2a4fdebc92e0c9eb38397443770b17af3
Attila Farago <attila.farago.hu@gmail.com>
2025-08-01 23:05:33 +0200

    refactor: streamline SVG to PNG conversion by consolidating font file paths

--- netlify/functions/imageHelper.ts
+++ netlify/functions/imageHelper.ts
@@ -5,29 +5,34 @@
 //     return pngBuffer.toString('base64');
 // }
 
-import { Resvg } from '@resvg/resvg-js';
+import { renderAsync, Resvg, ResvgRenderOptions } from '@resvg/resvg-js';
+import path from 'path';
+
+const fontsDir = path.join(__dirname, 'fonts');
+const fontFiles = [
+    'Arial.ttf',
+    'ChaletLondonNineteenSixty.ttf',
+    'ChaletNewYorkNineteenSixty.ttf',
+    'Helvetica.ttf',
+    'LiberationSans-Regular.ttf',
+].map((file) => path.join(fontsDir, file));
 
 export async function convertSvgToPngBase64(svg: string): Promise<string> {
-    const resvg = new Resvg(svg, {
+    const opts: ResvgRenderOptions = {
         fitTo: {
             mode: 'width',
             value: 1024,
         },
         font: {
-            fontFiles: [
-                './fonts/Arial.ttf',
-                './fonts/ChaletLondonNineteenSixty.ttf',
-                './fonts/ChaletNewYorkNineteenSixty.ttf',
-                './fonts/Helvetica.ttf',
-                './fonts/LiberationSans-Regular.ttf',
-            ],
-            fontDirs: ['./fonts', '../fonts', '/var/task/fonts'],
+            fontFiles,
+            // fontDirs: ['./fonts', '../fonts', '/var/task/fonts'],
             defaultFontFamily: 'sans-serif',
             sansSerifFamily: 'Helvetica',
         },
-    });
-
-    const pngData = resvg.render();
+    };
+    // const resvg = new Resvg(svg, opts);
+    // const pngData = resvg.render();
+    const pngData = await renderAsync(svg, opts);
     const pngBuffer = pngData.asPng();
 
     return pngBuffer.toString('base64');
