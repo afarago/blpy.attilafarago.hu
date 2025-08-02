54faf72bbb7e3780badfb488a1b456b604e53afc
Attila Farago <attila.farago.hu@gmail.com>
2025-08-02 18:44:40 +0200

    refactor: remove unused functions and streamline file handling in conversion process

--- netlify/functions/convert/imageHelper.ts
+++ netlify/functions/convert/imageHelper.ts
@@ -1,39 +1,39 @@
-// import sharp from "sharp";
-
-// export async function convertSvgToPngBase64(svg: string): Promise<string> {
-//     const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
-//     return pngBuffer.toString('base64');
-// }
-
-import { renderAsync, Resvg, ResvgRenderOptions } from '@resvg/resvg-js';
-import path from 'path';
-
-const fontsDir = path.join(__dirname, 'fonts');
-const fontFiles = [
-    'Arial.ttf',
-    'ChaletLondonNineteenSixty.ttf',
-    'ChaletNewYorkNineteenSixty.ttf',
-    'Helvetica.ttf',
-    'LiberationSans-Regular.ttf',
-].map((file) => path.join(fontsDir, file));
+import sharp from "sharp";
 
 export async function convertSvgToPngBase64(svg: string): Promise<string> {
-    const opts: ResvgRenderOptions = {
-        fitTo: {
-            mode: 'width',
-            value: 1024,
-        },
-        font: {
-            fontFiles,
-            // fontDirs: ['./fonts', '../fonts', '/var/task/fonts'],
-            defaultFontFamily: 'sans-serif',
-            sansSerifFamily: 'Helvetica',
-        },
-    };
-    // const resvg = new Resvg(svg, opts);
-    // const pngData = resvg.render();
-    const pngData = await renderAsync(svg, opts);
-    const pngBuffer = pngData.asPng();
-
+    const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
     return pngBuffer.toString('base64');
 }
+
+// import { renderAsync, Resvg, ResvgRenderOptions } from '@resvg/resvg-js';
+// import path from 'path';
+
+// const fontsDir = path.join(__dirname, 'fonts');
+// const fontFiles = [
+//     'Arial.ttf',
+//     'ChaletLondonNineteenSixty.ttf',
+//     'ChaletNewYorkNineteenSixty.ttf',
+//     'Helvetica.ttf',
+//     'LiberationSans-Regular.ttf',
+// ].map((file) => path.join(fontsDir, file));
+
+// export async function convertSvgToPngBase64(svg: string): Promise<string> {
+//     const opts: ResvgRenderOptions = {
+//         fitTo: {
+//             mode: 'width',
+//             value: 1024,
+//         },
+//         font: {
+//             fontFiles,
+//             // fontDirs: ['./fonts', '../fonts', '/var/task/fonts'],
+//             defaultFontFamily: 'sans-serif',
+//             sansSerifFamily: 'Helvetica',
+//         },
+//     };
+//     // const resvg = new Resvg(svg, opts);
+//     // const pngData = resvg.render();
+//     const pngData = await renderAsync(svg, opts);
+//     const pngBuffer = pngData.asPng();
+
+//     return pngBuffer.toString('base64');
+// }
